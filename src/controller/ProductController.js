8fe90ab968ac90f8dfe7919654049291edeb63dc
mahdiJihad <mahdijihad001@gmail.com>
2025-03-09 17:24:51 +0600

    Add Product Related Controller

---
+++ src/controller/ProductController.js
@@ -0,0 +1,47 @@
+
+// Get Brand Controller
+exports.BrandList = async(req , res) =>{
+
+};
+// Get Category Controller
+exports.CategoryList = async (req , res) => {
+    
+};
+
+// Get Slider Controller
+exports.SliderList = async (req , res) => {
+    
+}
+
+// List By Brand
+exports.ListByBrand = async (req , res) => {
+    
+};
+
+
+// List By Category
+exports.ListByCategory = async (req , res) => {
+    
+}
+
+// Product by similer
+exports.ProductBySimiler = async (req , res) => {
+    
+}
+
+
+// Product search 
+exports.ListByKeyword = async (req , res) => {
+    
+}
+
+
+// Single Product Review List
+exports.ProductRewiewList = async (req , res ) => {
+    
+}
+
+
+
+
+
