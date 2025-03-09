c89d0d07742a2c190d5048aa5887325b017e3395
mahdiJihad <mahdijihad001@gmail.com>
2025-03-09 22:36:24 +0600

    Add Product Services Demo Smpol

--- src/model/ProductModel.js
+++ src/model/ProductModel.js
@@ -20,5 +20,5 @@ const productSchema = mongoose.Schema({
 
 const productModel = mongoose.model("product" , productSchema);
 
-module.exports = {productModel};
+module.exports = productModel;
 
--- src/route/Api.js
+++ src/route/Api.js
@@ -1,6 +1,28 @@
 const express = require("express");
+const ProductController = require("../controller/ProductController")
 const router = express.Router();
 
+
+
+// ----------------------------Product Related Api ----------------------------------
+// Get Product  Brand List
+router.get("/productBrandList" , ProductController.ProductBrandList);
+// Get Product Category List
+router.get("/ProductCategoryList" , ProductController.ProductCategoryList);
+// Get Product Slider 
+router.get("/ProductSliderList" , ProductController.ProductSliderList);
+// Get All Product Specific Brand
+router.get("/ProductListByBrand/:brandID" , ProductController.ProductListByBrand);
+// // Get All Product Specific Category
+router.get("/ProductListByCategory/:categoryId" , ProductController.ProductListByCategory);
+// // Get All Smilier Product
+router.get("/ProductListBySmilier/:keyword" , ProductController.ProductListBySmilier);
+// Product Remark example Trending Top Etc...
+router.get("/ProductListByRemark/:remark" , ProductController.ProductListByRemark);
+// Porduct Details
+router.get("/ProductDetails/:productID" , ProductController.ProductDetails);
+// Product Review List by Product ID
+router.get("/ProductRewiewList/:productId" , ProductController.ProductRewiewList);
 
 
 
